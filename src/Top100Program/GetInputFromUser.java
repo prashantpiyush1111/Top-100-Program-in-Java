90ca56e161b71e9061229ec816b9e9c3bbcb8474
prashantpiyush1111 <Prashantpiyush1111@gmail.com>
2026-01-28 11:49:56 +0530

    2nd Day

--- src/Top100Program/GetInputFromUser.java
+++ src/Top100Program/GetInputFromUser.java
@@ -3,7 +3,7 @@ package Top100Program;
 import java.util.Scanner;
 
 public class GetInputFromUser {
-    public static void main(String args[]){
+    public static void main(String[]args){
         int a;
         float b;
         String s;
